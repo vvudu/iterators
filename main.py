996296b5ae6eb1879071ea8c06e5bc876d01eca9
Neto test <vvudu13@gmail.com>
2024-08-12 13:00:13 -1000

    3d test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 class FlatIterator:
+    """Итератор для списков с одним уровнем вложенности"""
     def __init__(self, list_of_list):
         self.list_of_list = list_of_list
         self.current_list = 0
@@ -18,9 +19,37 @@ class FlatIterator:
             return self.__next__()
         
         item = self.list_of_list[self.current_list][self.current_element]
-        print(f"{item}")
+        print(f"FI item: {item}")
         self.current_element += 1
         return item
+    
+class DeepFlatIterator:
+    """Итератор для списков с любым уровнем вложенности"""
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+        self.stack = [(list_of_list, 0)]  # Стек для хранения текущего списка и индекса
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.stack:
+            current_list, current_index = self.stack[-1]
+
+            if current_index >= len(current_list):
+                self.stack.pop()
+                continue
+
+            self.stack[-1] = (current_list, current_index + 1)
+            item = current_list[current_index]
+
+            if isinstance(item, list):
+                self.stack.append((item, 0))
+            else:
+                print(f"DFI item: {item}")
+                return item
+
+        raise StopIteration
 
 def test_1():
 
@@ -40,7 +69,25 @@ def test_1():
     assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
 
 
+def test_3():
+
+    list_of_lists_2 = [
+        [['a'], ['b', 'c']],
+        ['d', 'e', [['f'], 'h'], False],
+        [1, 2, None, [[[[['!']]]]], []]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            DeepFlatIterator(list_of_lists_2),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+    ):
+        
+        assert flat_iterator_item == check_item
+
+    assert list(DeepFlatIterator(list_of_lists_2)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None, '!']
+
 
 
 if __name__ == '__main__':
     test_1()
+    test_3()
